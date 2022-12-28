6406979354384103139c957b85635584c9e42a72
ivmerk <ivan.e.merkulov@gmail.com>
2022-12-28 17:08:16 +0200

    2.11. done

--- src/common/file-reader/tsv-file-reader.ts
+++ src/common/file-reader/tsv-file-reader.ts
@@ -1,42 +1,35 @@
-import { readFileSync } from 'fs';
-import { MovieCard} from '../../types/moviecard.type.js';
-import { Staring} from '../../types/staring.type.js';
-import { Genres} from '../../types/genres.type.js';
+import EventEmitter from 'events';
+import { createReadStream } from 'fs';
 import { FileReaderInterface } from './file-reader.interface.js';
 
-export default class TSVFileReader implements FileReaderInterface {
-  private rawData = '';
-
-  constructor(public filename: string) { }
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf8' });
+export default class TSVFileReader extends EventEmitter implements FileReaderInterface {
+  constructor(public filename: string) {
+    super();
   }
 
-  public toArray(): MovieCard[] {
-    if (!this.rawData) {
-      return [];
+  public async read(): Promise<void> {
+    const stream = createReadStream(this.filename, {
+      highWaterMark: 16384, // 16KB
+      encoding: 'utf-8',
+    });
+
+    let lineRead = '';
+    let endLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of stream) {
+      lineRead += chunk.toString();
+
+      while ((endLinePosition = lineRead.indexOf('\n')) >= 0) {
+        const completeRow = lineRead.slice(0, endLinePosition + 1);
+        lineRead = lineRead.slice(++endLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
     }
 
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim() !== '')
-      .map((line) => line.split('\t'))
-      .map(([title, description, postDate, genres, released, rating, previewVideoLink, videoLink, staring, director, runTime, posterImage, backgroundImage, backgroundColor]) => ({
-        title,
-        description,
-        postDate: new Date(postDate),
-        genres: <Genres[]>genres.split(';').map((genre) => ({ genre })),
-        released: Number.parseInt(released, 10),
-        rating: Number.parseInt(rating, 10),
-        previewVideoLink,
-        videoLink,
-        staring: <Staring[]>staring.split(';').map((name) => ({name})),
-        director,
-        runTime: Number.parseInt(runTime, 10),
-        posterImage,
-        backgroundImage,
-        backgroundColor
-      }));
+    this.emit('end', importedRowCount);
   }
+
 }
---
+++ src/common/movie-card-generator/movie-card-generator.ts
@@ -0,0 +1,37 @@
+import { RATING_COUNT, ReleasedYear, RunTime } from '../../const.js';
+import { MockData } from '../../types/mock-data.type.js';
+import { generateRandomValue, getRandomItems } from '../../utils/random.js';
+import { getRandomItem } from '../../utils/random.js';
+import { MovieCardGeneratorInterface } from './movie-card-generator.interface.js';
+import dayjs from 'dayjs';
+
+const FIRST_WEEK_DAY = 1;
+const LAST_WEEK_DAY = 7;
+
+export default class MovieCardGenerator implements MovieCardGeneratorInterface{
+  constructor(private readonly mockData: MockData) {}
+
+  public generate(): string {
+    const title = getRandomItem<string>(this.mockData.title);
+    const description = getRandomItem<string>(this.mockData.description);
+    const postDate = dayjs().subtract(generateRandomValue(FIRST_WEEK_DAY, LAST_WEEK_DAY), 'day').toISOString();
+    const genres = getRandomItems<string>(this.mockData.genres).join(';');
+    const released = generateRandomValue(ReleasedYear.Min, ReleasedYear.Max).toString();
+    const rating = generateRandomValue(0, RATING_COUNT).toString();
+    const previewVideoLink = getRandomItem<string>(this.mockData.previewVideoLink);
+    const videoLink = getRandomItem<string>(this.mockData.videoLink);
+    const staring = getRandomItems<string>(this.mockData.staring).join(';');
+    const director = getRandomItem<string>(this.mockData.director);
+    const runTime = generateRandomValue(RunTime.Min, RunTime.Max).toString();
+    const posterImage = getRandomItem<string>(this.mockData.posterImage);
+    const backgroundImage = getRandomItem<string>(this.mockData.backgroundImage);
+    const backgroundColor = getRandomItem<string>(this.mockData.backgroundColor);
+
+
+    return [
+      title, description, postDate, genres, released, rating, previewVideoLink,
+      videoLink, staring, director, runTime, posterImage,
+      backgroundImage, backgroundColor
+    ].join('\t');
+  }
+}
